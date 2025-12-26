bef422a65bd5ae76d200e817e5acf576fe5408bd
Soulhae <herrera.benjamin@hotmail.com>
2025-12-25 21:47:05 -0300

    dodge

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
-@export var speed = 250
+var speed = 250
 @export var bulletScene: PackedScene
+var dodge_duration = 0.75
+var dodge_cooldown = 2
+var can_dodge = true
+var is_dodging = false
 
 func _init():
 	position.x = 324
 	position.y = 64
+
 
 func _physics_process(_delta: float) -> void:
 	var moving = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -16,8 +21,8 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 	
 	var dashing = Input.is_action_just_pressed("dash")
-	if dashing:
-		position += velocity * 0.35
+	if dashing and can_dodge:
+		start_dodge()
 	
 	var shooting = Input.is_action_just_pressed("test")
 	if shooting:
@@ -26,6 +31,32 @@ func _physics_process(_delta: float) -> void:
 		bullet.position.y = position.y + 32
 		get_parent().add_child(bullet)
 		
-	
 	position.x = clamp(position.x, 0, get_viewport_rect().size.x)
 	position.y = clamp(position.y, 0, get_viewport_rect().size.y)
+
+
+func start_dodge():
+	can_dodge = false
+	is_dodging = true
+	
+	set_collision_layer_value(1, false)
+	
+	var tween = create_tween().set_parallel(true)
+	tween.tween_property($Sprite2D,"scale",Vector2(0.75,0.75), 0.1)
+	tween.tween_property($Sprite2D,"modulate",Color(0.5,0.5,0.5,0.995),0.1)
+	
+	await get_tree().create_timer(dodge_duration).timeout
+	end_dodge()
+
+
+func end_dodge():
+	is_dodging = false
+	
+	set_collision_layer_value(1, true)
+	
+	var tween = create_tween().set_parallel(true)
+	tween.tween_property($Sprite2D, "scale", Vector2(1.0, 1.0), 0.1)
+	tween.tween_property($Sprite2D, "modulate", Color(1, 1, 1, 1), 0.1)
+	
+	await get_tree().create_timer(dodge_cooldown).timeout
+	can_dodge = true
