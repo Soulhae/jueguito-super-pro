0c22f5020c890fb51a0926a531f97da38c6cd2d1
Soulhae <herrera.benjamin@hotmail.com>
2025-12-25 23:52:45 -0300

    no me acuerdo q hice

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -8,6 +8,7 @@ func _physics_process(delta):
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
-func _on_body_entered(boss):
-	boss.hp_loss()
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_method("hp_loss"):
+		body.hp_loss()
 	queue_free()
---
+++ Scripts/game_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+const PLAYFIELD_MARGIN_LEFT = 444
+const PLAYFIELD_MARGIN_RIGHT = 1092
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
